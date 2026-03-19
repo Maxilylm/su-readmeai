4750d472c57ebbd824a4f9708df30554c772d280
Maxilylm <maxilyldiaz@gmail.com>
2026-03-19 14:54:27 -0300

    feat: add SlopAds cross-promotion banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}        <script src="https://78slopads.vercel.app/api/promo.js" defer></script>
+      </body>
     </html>
   );
 }
